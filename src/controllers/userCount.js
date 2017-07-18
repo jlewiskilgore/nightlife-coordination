6c4fb595f1e028a6c5c75a780c0d1108a166a05e
Lewis Kilgore <jlewis.kilgore@gmail.com>
2017-07-17 20:57:38 -0400

    Create queryDate helper function and function to return count for user by locationList

--- src/controllers/userCount.js
+++ src/controllers/userCount.js
@@ -3,13 +3,7 @@ function userCount(locationList) {
 		var db = req.db;
 		var usergoings = db.collection('usergoings');
 
-		var timezoneOffset = (new Date()).getTimezoneOffset() * 60000;
-		var localDate = new Date(Date.now() - timezoneOffset);
-
-		queryDate = localDate.setHours(5);
-		queryDate = localDate.setMinutes(0);
-		queryDate = localDate.setSeconds(0);
-		queryDate = new Date(queryDate);
+		var queryDate = this.getQueryDate();
 
 		var searchQuery = {
 			"locationId": location.id,
@@ -26,6 +20,42 @@ function userCount(locationList) {
 		});
 	};
 
+	this.isCurrentUserGoing = function(req, location, resultIndex, userId, callback) {
+		var db = req.db;
+		var usergoings = db.collection('usergoings');
+
+		var queryDate = this.getQueryDate();
+
+		var searchQuery = {
+			"locationId": location.id,
+			"userId": userId,
+			"dateGoing": { "$gte": queryDate }
+		}
+
+		usergoings.count(searchQuery, function(err, result) {
+			if(err) {
+				console.log(err);
+			}
+			else {
+				callback(result, resultIndex);
+			}
+		})
+	};
+
+
+	// TODO: Return list of all location by user by queryDate
+	this.currentUserGoingList = function(req, currentUser, callback) {
+		var db = req.db;
+		var usergoings = db.collection('usergoings');
+
+		var queryDate = this.getQueryDate();
+
+		var searchQuery = {
+			"userId": userId,
+			"dateGoing": { "$gte": queryDate }
+		}
+	};
+
 	this.countLocationList = function(req, locationList, callback) {
 		var usersAttendingArr = [];
 		var resultLength = 0;
@@ -40,6 +70,34 @@ function userCount(locationList) {
 				}
 			});
 		}
+	};
+
+	this.currentUserGoingByLocation = function(req, locationList, currentUser, callback) {
+		var currentUserAttendingArr = [];
+		var resultLength = 0;
+
+		for(var i=0; i < locationList.length; i++) {
+			this.isCurrentUserGoing(req, locationList[i], i, user, function(isUserGoing, locationIndex) {
+				currentUserAttendingArr[locationIndex] = isUserGoing;
+				resultLength++;
+
+				if(resultLength == locationList.length) {
+					callback(currentUserAttendingArr);
+				}
+			});
+		}
+	};
+
+	this.getQueryDate = function () {
+		var timezoneOffset = (new Date()).getTimezoneOffset() * 60000;
+		var localDate = new Date(Date.now() - timezoneOffset);
+
+		queryDate = localDate.setHours(5);
+		queryDate = localDate.setMinutes(0);
+		queryDate = localDate.setSeconds(0);
+		queryDate = new Date(queryDate);
+
+		return queryDate;
 	};
 }
 
